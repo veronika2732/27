f01ffdd0391eb3983c4a478ee00eb3ed1c54594d
Veronika <tokareva_v@inbox.ru>
2022-10-18 19:16:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int sum (int n)
+{
+    int res = 0;
+    while (n > 0)
+    {
+        res = res + (n % 10);
+        n = n / 10;
+    }
+    return res;
+}
+Console.Clear ();
+Console.White ("Введите число:");
+int N = int.Parse (Console.ReadLine()!);
+Console.WriteLine ($"Сумма цтфрчисла {N} равна {Sum(N)}");
